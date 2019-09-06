9c2307fe01151fb76a6eb9aa135e186be0b85ec9
hugues <ghugues@netc.fr>
2019-09-06 22:15:42 +0200

    main.go: add delay

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"./doc"
 	"log"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	defer log.Print("TIME: ",time.Since(start))
 	log.SetPrefix("==== ")
 	log.SetFlags(0)
 	ind := doc.Read("dataTest/")
