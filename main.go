96746c32d166677d9bd6ab7b6d4356267e77788e
hugues <ghugues@netc.fr>
2019-09-07 14:04:41 +0200

    main.go: rm time

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"./doc"
 	"log"
-	"time"
 )
 
 func main() {
-	start := time.Now()
-	defer log.Print("TIME: ",time.Since(start))
 	log.SetPrefix("==== ")
 	log.SetFlags(0)
 	ind := doc.Read("dataTest/")
