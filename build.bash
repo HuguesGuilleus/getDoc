27e5061c30a23476f3733be72b49d5560ba2b06a
hugues <ghugues@netc.fr>
2019-09-08 14:01:54 +0200

    style: add style and innclude sys in build

--- build.bash
+++ build.bash
@@ -2,13 +2,22 @@
 
 clear
 
+# Style
+cat style/*.css | tr '\t\n' ' ' |
+	# sed -E 's#[^/]\*[^/]##g' | sed -E 's#/\*[^*]*\*/# #g' |
+	sed -E 's# +# #g' | sed -E 's#([:;{}]) #\1#g' |
+	sed 's#^#\t<style>#' | sed 's#$#</style>\n#' > style.html
+
 # Template
 cd doc/data/
 printf "package data
 var index = \`" > index.go
-cat index.gohtml >> index.go
+sed '/<!--CODE-->/r ../../style.html' < index.gohtml |
+	sed '/<!--CODE-->/d' >>index.go
 echo '`' >> index.go
 cd ../..
+
+rm style.html
 
 # Build
 go build
