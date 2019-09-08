522b2f9eb2f7b53fcac4361efded4d7e85495580
hugues <ghugues@netc.fr>
2019-09-08 23:54:36 +0200

    web: search in Title, lang, fileRef and type

--- search/search.js
+++ search/search.js
@@ -1,17 +1,19 @@
 (function () {
-	h1 = document.querySelector("h1"); ///////// for dev
 	searchInput = document.getElementById("searchInput");
-	searchInput.oninput = ()=>{
-		arg = searchParse(searchInput.value);
-		if (arg === null) {
-			searchReset();
-		} else if (arg.cmd.length > 0) {
-			searchAct(arg)
-		} else {
-			searchElement(arg);
-		}
-	}
+	searchInput.oninput = search ;
+	window.onload = search ;
 })();
+
+function search() {
+	arg = searchParse(searchInput.value);
+	if (arg === null) {
+		searchReset();
+	} else if (arg.cmd.length > 0) {
+		searchAct(arg)
+	} else {
+		searchElement(arg);
+	}
+}
 
 // Parse the string from inout element and return an object
 function searchParse(input) {
@@ -37,10 +39,13 @@ function searchParse(input) {
 				}
 			} else {
 				let cat = false ;
+				let val ;
 				if (/\w+:.*/.test(el)) {
 					cat = el.replace(/(\w+):.*/, "$1");
+					val = el.replace(/^\w+:(.*)/, "$1");
+				} else {
+					val = el;
 				}
-				let val = el.replace(/:?(.*)/, "$1");
 				switch (cat) {
 					case "file":arg.file.push(val);break;
 					case "lang":arg.lang.push(val);break;
@@ -65,34 +70,44 @@ function searchReset() {
 	document.getElementById("std").hidden = false ;
 	document.getElementById("searchResult").hidden = true ;
 	for (let item of document.querySelectorAll("#list>li")) {
-		let name = item.querySelector(".elementTitle");
-		name.innerHTML = name.textContent ;
 		item.hidden = false ;
+		searchResetElement(item.querySelector(".fileRef"));
+		searchResetElement(item.querySelector(".lang"));
+		searchResetElement(item.querySelector(".elementTitle"));
+		searchResetElement(item.querySelector(".type"));
 	}
+}
+
+// Reset an simple elem
+function searchResetElement(el) {
+	el.innerHTML = el.textContent ;
 }
 
 // Search in the list of element, hidden the not found item
 function searchElement(arg) {
-	console.log("searchElement");
 	document.getElementById("std").hidden = false ;
 	document.getElementById("searchResult").hidden = true ;
 	for (let item of document.querySelectorAll("#list>li")) {
-		let notFound = true
-		let name = item.querySelector(".elementTitle");
-		notFound = notFound && searchMatch(name, arg.all.concat(arg.name)) ;
-		item.hidden = notFound ;
+		let notFound = 0 ;
+		notFound += searchMatch(item.querySelector(".fileRef"),arg.file,arg.all);
+		notFound += searchMatch(item.querySelector(".lang"),arg.lang,arg.all);
+		notFound += searchMatch(item.querySelector(".elementTitle"),arg.name,arg.all);
+		notFound += searchMatch(item.querySelector(".type"),arg.type,arg.all);
+		item.hidden = notFound < 1 ;
 	}
 }
 
 // Print the matched string (from the array of string) in the element.
-// It return true if no matched and false if match.
-function searchMatch(el,tab) {
-	if (tab.length === 0) {
-		el.innerHTML = el.textContent ;
-		return true ;
+// It return -10 if the tab if not empty and the pattern not found,
+// return 1 if there are a match, and return 0 if tab is empty and not found
+function searchMatch(el,tab, all) {
+	var pattern = new RegExp("("+tab.concat(all).join("|")+")", "gui");
+	el.innerHTML = el.textContent.replace(pattern,'<span class=find>$1</span>') ;
+	if (pattern.test(el.textContent)) {
+		return 1 ;
+	} else if (tab.length !== 0) {
+		return -10 ;
 	} else {
-		var pattern = new RegExp("("+tab.join("|")+")", "g");
-		el.innerHTML = el.textContent.replace(pattern,'<span class=find>$1</span>') ;
-		return !pattern.test(el.textContent) ;
+		return 0 ;
 	}
 }
