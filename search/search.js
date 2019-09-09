c8f8cc419205c13ed8942adbdd1d7a7e0bf04d88
hugues <ghugues@netc.fr>
2019-09-09 12:42:16 +0200

    search: the regexp were create in searchParse

--- search/search.js
+++ search/search.js
@@ -1,3 +1,6 @@
+// The parsed elements of search
+var arg = null ;
+
 (function () {
 	searchInput = document.getElementById("searchInput");
 	searchInput.oninput = search ;
@@ -11,16 +14,19 @@ function search() {
 	} else if (arg.cmd.length > 0) {
 		searchAct(arg)
 	} else {
-		searchElement(arg);
+		searchElement();
 	}
 }
+
+// The patern for parsing input element
+const searchPattern = /^(?:(\w*):)?(.*)$/ ;
 
 // Parse the string from inout element and return an object
 function searchParse(input) {
 	if (input.length === 0) {
 		return null
 	} else {
-		var arg = {
+		var list = {
 			file:[],
 			lang:[],
 			name:[],
@@ -29,35 +35,36 @@ function searchParse(input) {
 			cmd:[],
 		}
 		for (let el of input.split(/\s+/)) {
-			if (el === "") {
-				continue
-			}
-			if (el[0] === "$") {
-				let r = el.replace(/\$(\w*).*/, "$1")
-				if (r) {
-					arg.cmd.push(r)
-				}
-			} else {
-				let cat = false ;
-				let val ;
-				if (/\w+:.*/.test(el)) {
-					cat = el.replace(/(\w+):.*/, "$1");
-					val = el.replace(/^\w+:(.*)/, "$1");
-				} else {
-					val = el;
-				}
-				switch (cat) {
-					case "file":arg.file.push(val);break;
-					case "lang":arg.lang.push(val);break;
-					case "name":arg.name.push(val);break;
-					case "type":arg.type.push(val);break;
-					case false:
-						arg.all.push(val);
-						break;
-				}
+			var cat = el.replace(searchPattern, "$1")
+			var val = el.replace(searchPattern, "$2")
+			if (val === "") continue ;
+			switch (cat) {
+				case "file":list.file.push(val);break;
+				case "lang":list.lang.push(val);break;
+				case "name":list.name.push(val);break;
+				case "type":list.type.push(val);break;
+				case "":    list.all.push(val);break;
 			}
 		}
-		return arg
+		return {
+			cmd: list.cmd,
+			file: {
+				pat: new RegExp("("+list.file.concat(list.all).join("|")+")", "gui"),
+				notFound: list.file.length ,
+			},
+			lang: {
+				pat: new RegExp("("+list.lang.concat(list.all).join("|")+")", "gui"),
+				notFound: list.lang.length ,
+			},
+			name: {
+				pat: new RegExp("("+list.name.concat(list.all).join("|")+")", "gui"),
+				notFound: list.name.length ,
+			},
+			type: {
+				pat: new RegExp("("+list.type.concat(list.all).join("|")+")", "gui"),
+				notFound: list.type.length ,
+			},
+		}
 	}
 }
 
@@ -80,34 +87,34 @@ function searchReset() {
 
 // Reset an simple elem
 function searchResetElement(el) {
-	el.innerHTML = el.textContent ;
+	if (el.querySelectorAll(".find").length > 0) {
+		el.innerHTML = el.textContent ;
+	}
 }
 
 // Search in the list of element, hidden the not found item
-function searchElement(arg) {
+function searchElement() {
 	document.getElementById("std").hidden = false ;
 	document.getElementById("searchResult").hidden = true ;
 	for (let item of document.querySelectorAll("#list>li")) {
-		let notFound = 0 ;
-		notFound += searchMatch(item.querySelector(".fileRef"),arg.file,arg.all);
-		notFound += searchMatch(item.querySelector(".lang"),arg.lang,arg.all);
-		notFound += searchMatch(item.querySelector(".elementTitle"),arg.name,arg.all);
-		notFound += searchMatch(item.querySelector(".type"),arg.type,arg.all);
-		item.hidden = notFound < 1 ;
+		item.hidden = (
+			searchMatch(item.querySelector(".fileRef"),arg.file)
+			+ searchMatch(item.querySelector(".lang"),arg.lang)
+			+ searchMatch(item.querySelector(".elementTitle"),arg.name)
+			+ searchMatch(item.querySelector(".type"),arg.type)
+		) < 1 ;
 	}
 }
 
 // Print the matched string (from the array of string) in the element.
 // It return -10 if the tab if not empty and the pattern not found,
 // return 1 if there are a match, and return 0 if tab is empty and not found
-function searchMatch(el,tab, all) {
-	var pattern = new RegExp("("+tab.concat(all).join("|")+")", "gui");
-	el.innerHTML = el.textContent.replace(pattern,'<span class=find>$1</span>') ;
-	if (pattern.test(el.textContent)) {
-		return 1 ;
-	} else if (tab.length !== 0) {
-		return -10 ;
+function searchMatch(el, finder ) {
+	searchResetElement(el)
+	if (finder.pat.test(el.textContent)) {
+		el.innerHTML = el.textContent.replace(finder.pat,'<span class=find>$1</span>');
+		return 1
 	} else {
-		return 0 ;
+		return -10 * finder.notFound ;
 	}
 }
---
+++ search/test.js
@@ -0,0 +1,53 @@
+// DEVELOPPMENT TOOLS //
+
+// Function to test the speed of searchElement()
+function benchSearchElement(nb) {
+	nb = Number(nb);
+	if (isNaN(nb)) {
+		throw new TypeError("nb is not a number")
+	}
+	var begin = (new Date()).getTime();
+	for (var i = 0; i < nb; i++) {
+		arg = searchParse(searchInput.value);
+		searchElement();
+	}
+	return ( (new Date()).getTime() - begin )/nb ;
+}
+
+function testSearchPattern() {
+	var input = [
+		{
+			inp: "aaa:bbb",
+			cat: "aaa",
+			val: "bbb",
+		},
+		{
+			inp: "aaa:",
+			cat: "aaa",
+			val: "",
+		},
+		{
+			inp: "bbb",
+			cat: "",
+			val: "bbb",
+		},
+	];
+	let i = 0;
+	let allValidate = true;
+	for (let t of input) {
+		if (t.inp.replace(searchPattern,"$1") !== t.cat) {
+			allValidate = false ;
+			console.log(i,"cat:", t.cat, t.inp.replace(searchPattern,"$1"))
+		}
+		if (t.inp.replace(searchPattern,"$2") !== t.val) {
+			allValidate = false ;
+			console.log(i,"val:", t.val, t.inp.replace(searchPattern,"$2"))
+		}
+		i++;
+	}
+	if (allValidate) {
+		console.log("Test réussi");
+	} else {
+		console.log("test échoué");
+	}
+}
