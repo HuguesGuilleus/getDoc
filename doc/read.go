60b8e85811fd430d13548426652b7101722190ad
hugues <ghugues@netc.fr>
2019-09-14 09:55:18 +0200

    doc/read: use filepath.Walk

--- doc/read.go
+++ doc/read.go
@@ -1,7 +1,6 @@
 package doc
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,58 +10,30 @@ import (
 var Title string = ""
 
 // Read a file or a directory
-func Read(path string) (ind *Index) {
+func Read(root string) (ind *Index) {
 	ind = &Index{}
-	blobInfo, err := os.Stat(path)
-	if err != nil {
-		printErr(err)
-		return
-	}
 	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	if blobInfo.IsDir() {
-		go ind.readDir(path, wg)
-		t, err := filepath.Abs(filepath.Dir(path))
-		printErr(err)
-		Title = filepath.Base(t)
-	} else {
-		go ind.readFile(path, wg)
-		Title = blobInfo.Name()
-	}
-	wg.Wait()
-	return
-}
-
-// read a dir and for each file call readDir or readFile
-func (ind *Index) readDir(path string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	if path[len(path)-1] != '/' {
-		path += "/"
-	}
-	// log.Print("READ DIRECTORY: ", path)
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		log.Print("Error", err)
-		return
-	}
-	for _, file := range files {
-		wg.Add(1)
-		if file.IsDir() {
-			go ind.readDir(path+file.Name(), wg)
+	defer wg.Wait()
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		defer rec()
+		panicing(err)
+		if info.IsDir() {
+			return nil
 		} else {
-			go ind.readFile(path+file.Name(), wg)
+			wg.Add(1)
+			go ind.readFile(path, wg)
 		}
-	}
+		return nil
+	})
+	return ind
 }
 
 // Read one file, test if the type is know, split the line and call the parser
 func (ind *Index) readFile(path string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	defer rec()
 	if parser := langKnown(getExt(path)); parser != nil {
 		log.Print("READ FILE: ", path)
-		lines := splitFile(path)
-		parser(ind, lines, path)
+		parser(ind, splitFile(path), path)
 	}
 }
 
