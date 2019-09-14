cd1ba978b35fd6a56a761d29355ccaa12a53da3b
hugues <ghugues@netc.fr>
2019-09-14 14:12:23 +0200

    main.go: parse the argument

--- main.go
+++ main.go
@@ -2,16 +2,92 @@ package main
 
 import (
 	"./doc"
+	"fmt"
+	"github.com/HuguesGuilleus/parseOpt"
 	"log"
 	"os"
 )
 
-func main() {
+var spec = &parseOpt.SpecList{
+	&parseOpt.Spec{
+		NameShort: "v",
+		NameLong:  "version",
+		Desc:      "Print the version",
+		CBFlag: func() {
+			log.Printf("VERSION: %.2f\n", 1.02)
+			os.Exit(0)
+		},
+	},
+	&parseOpt.Spec{
+		NameShort: "l",
+		NameLong:  "lang",
+		Desc:      "List all supported programming languages",
+		CBFlag: func() {
+			log.Println("LANGUAGE:")
+			for _, lang := range []string{"bash", "c", "go", "js"} {
+				fmt.Println("  -", lang)
+			}
+			fmt.Println("  More datail on Github")
+			os.Exit(0)
+		},
+	},
+	&parseOpt.Spec{
+		NameShort:  "o",
+		NameLong:   "output",
+		Desc:       "Set html output file",
+		OptionName: "file.html",
+		NeedArg:    true,
+	},
+	&parseOpt.Spec{
+		NameShort:  "j",
+		NameLong:   "json",
+		OptionName: "file.json",
+		Desc:       "Set json output file",
+		NeedArg:    true,
+	},
+	&parseOpt.Spec{
+		NameShort:  "x",
+		NameLong:   "xml",
+		OptionName: "file.xml",
+		Desc:       "Set xml output file",
+		NeedArg:    true,
+	},
+}
+
+func init() {
 	log.SetPrefix("==== ")
 	log.SetFlags(0)
 	log.SetOutput(os.Stdout)
-	ind := doc.Read("./")
-	ind.SaveHTML("./doc.html")
-	ind.DataIndex().Json("doc.json")
-	ind.DataIndex().Xml("doc.xml")
+}
+
+func main() {
+	opt := spec.ParseOs()
+	// Read file
+	var ind *doc.Index
+	if len(opt.Option[""]) == 0 {
+		ind = doc.Read(".")
+	} else {
+		ind = &doc.Index{}
+		for _, file := range opt.Option[""] {
+			*ind = append(*ind, *doc.Read(file)...)
+		}
+	}
+	// Save the HTML
+	if len(opt.Option["output"]) != 0 {
+		for _, out := range opt.Option["output"] {
+			ind.SaveHTML(out)
+		}
+	} else if len(opt.Option["json"]) == 0 && len(opt.Option["xml"]) == 0 {
+		ind.SaveHTML("doc.html")
+	}
+	// Save in JSON and XML
+	if len(opt.Option["json"]) != 0 || len(opt.Option["xml"]) != 0 {
+		data := ind.DataIndex()
+		for _, file := range opt.Option["json"] {
+			data.Json(file)
+		}
+		for _, file := range opt.Option["xml"] {
+			data.Xml(file)
+		}
+	}
 }
