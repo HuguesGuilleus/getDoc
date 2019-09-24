bbe05b23f29d17f0dd13594775f0b7169be00bec
hugues <ghugues@netc.fr>
2019-09-24 17:39:42 +0200

    fix: auto focus searchInput crt and alt

--- search/key.js
+++ search/key.js
@@ -3,8 +3,10 @@
 // BSD 3-Clause "New" or "Revised" License
 
 window.addEventListener("keydown", event=>{
+	if (event.ctrlKey || event.altKey) {
+		return ;
+	}
 	if (document.activeElement != searchInput && event.keyCode > 30) {
-		searchInput.value += event.key ;
 		searchInput.focus();
 	}
 });
